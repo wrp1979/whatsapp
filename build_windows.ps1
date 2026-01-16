11dcea28569e46c4bc5ec7ee2206f126dcf60bc7
Your Name <you@example.com>
2026-01-16 18:10:13 -0300

    build: pass MSVC __cplusplus flag to qmake

--- build_windows.ps1
+++ build_windows.ps1
@@ -17,6 +17,6 @@ if (-not $qmake) {
 }
 
 Push-Location $buildPath
-& $qmake.Source (Join-Path $root "src\\WhatsApp.pro") "CONFIG+=$Config" "QMAKE_CXXFLAGS+=/Zc:__cplusplus"
+& $qmake.Source (Join-Path $root "src\\WhatsApp.pro") "CONFIG+=$Config" "QMAKE_CXXFLAGS+=-Zc:__cplusplus" "QMAKE_CXXFLAGS_RELEASE+=-Zc:__cplusplus"
 & nmake
 Pop-Location
